27a59f94a822dc20e556d0d539bc713c5ca7b6ea
istefanoff <official.stefanov@gmail.com>
2022-01-20 10:01:40 +0200

    Update MedenkaWars

--- MedenkaWars.java
+++ MedenkaWars.java
@@ -1,3 +1,5 @@
+package com.company;
+
 import java.util.Scanner;
 
 public class MedenkaWars {
@@ -9,64 +11,68 @@ public class MedenkaWars {
 
         //1 medenka - 60 damage
 
-        double naskorDamageDealt = 0;
-        double vitkorDamageDealt = 0;
+        double naskorMedenkas = 0;
+        double vitkorMedenkas = 0;
         int naskorAttackCount = 0;
         int vitkorAttackCount = 0;
-        double naskorLastDamageAmount = 0;
-        double vitkorLastDmgAmount = 0;
+        double naskorLastMedenkas = 0;
+        double vitkorLastMedenkas = 0;
         double currentAttackV = 0;
         double currentAttackN = 0;
 
         String command = scanner.nextLine();
-
+        boolean isConsecutive = false;
         while(!command.equals("end")){
         String[] data = command.split("\\s+");
             int medenkaCount = Integer.parseInt(data[0]);
             switch (data[1]){
                 case "white": // Vitkor attacks
-                    if (vitkorAttackCount == 2){
-                        vitkorDamageDealt += (medenkaCount * 60) * 2.75;
+                    if (vitkorAttackCount == 2 && vitkorLastMedenkas == medenkaCount){
+                        vitkorMedenkas += medenkaCount * 2.75;
                         vitkorAttackCount = 0;
+                        vitkorLastMedenkas = medenkaCount * 2.75;
+                    }else {
+                        vitkorMedenkas += medenkaCount;
+                        vitkorLastMedenkas = medenkaCount;
                     }
-                    currentAttackV = medenkaCount * 60;
-                    vitkorDamageDealt += currentAttackV;
 
-                    if (currentAttackV == vitkorLastDmgAmount || vitkorLastDmgAmount == 0){
+                    if (medenkaCount == vitkorLastMedenkas || vitkorLastMedenkas == 0){
                         vitkorAttackCount++;
                     }else {
                         vitkorAttackCount = 0;
                     }
-                    vitkorLastDmgAmount = currentAttackV;
+
 
                     break;
+
                 case "dark":  // Naskor attacks
-                    currentAttackN = medenkaCount * 60;
-                    if (currentAttackN == naskorLastDamageAmount || naskorLastDamageAmount ==0){
+                    if (naskorAttackCount == 4){
+                        naskorMedenkas +=  medenkaCount * 4.5;
+                        naskorLastMedenkas = medenkaCount * 4.5;
+                        isConsecutive = true;
+                    }else {
+                        naskorMedenkas += medenkaCount;
+                        naskorLastMedenkas = medenkaCount;
+                    }
+
+                    if (medenkaCount == naskorLastMedenkas || naskorLastMedenkas ==0){
                         naskorAttackCount++;
+                    }else if (isConsecutive){
+                        naskorAttackCount = 1;
+                        isConsecutive = false;
                     }else {
                         naskorAttackCount = 0;
                     }
-                    if (naskorAttackCount == 5){
-                        naskorDamageDealt += currentAttackN * 4.5;
-                        naskorAttackCount = 1;
-                    }else {
-                        naskorDamageDealt += currentAttackN;
-                        naskorLastDamageAmount = currentAttackN;
-                    }
-
-
                     break;
             }
             command = scanner.nextLine();
         }
-
-        if (vitkorDamageDealt > naskorDamageDealt){
+        if (vitkorMedenkas > naskorMedenkas){
             System.out.println("Winner - Vitkor");
-            System.out.printf("%.0f", vitkorDamageDealt);
+            System.out.printf("%.0f", vitkorMedenkas * 60);
         }else {
             System.out.println("Naskor");
-            System.out.printf("%.0f", naskorDamageDealt);
+            System.out.printf("%.0f", naskorMedenkas * 60);
         }
     }
 }
