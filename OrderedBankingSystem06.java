30ab763686f9810a2c58329710c4d0e003e99b82
istefanoff <official.stefanov@gmail.com>
2022-01-24 14:59:44 +0200

    up

--- OrderedBankingSystem06.java
+++ OrderedBankingSystem06.java
@@ -3,7 +3,6 @@ package Jan21;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class OrderedBankingSystem06 {
     public static void main(String[] args) {
@@ -17,7 +16,7 @@ public class OrderedBankingSystem06 {
             String[] data = input.split(" -> ");
             String bank = data[0];
             String account = data[1];
-            Double balance = Double.parseDouble(data[2]);
+            double balance = Double.parseDouble(data[2]);
 
             banks.putIfAbsent(bank,new LinkedHashMap<>());
 
@@ -30,7 +29,6 @@ public class OrderedBankingSystem06 {
 
             input = scanner.nextLine();
         }
-        AtomicReference<String> lastElement = new AtomicReference<>("");
         banks.entrySet().stream().sorted((e1,e2) -> {
             double e2bankSum = e2.getValue().values().stream().mapToDouble(d -> d).sum();
             double e1bankSum = e1.getValue().values().stream().mapToDouble(d -> d).sum();
@@ -42,28 +40,9 @@ public class OrderedBankingSystem06 {
             }
             return result;
         }).forEach(e -> {
-
-            if (e.getValue().keySet().iterator().hasNext()){
                 System.out.println(e.getValue().keySet().iterator().next() + " -> " + e.getValue().values().iterator().next() + "(" +e.getKey() + ")");
-            }else {
-                System.out.println("stack is empty"+ " -> " + e.getValue().values().iterator().next() + "(" +e.getKey() + ")");
-            }
-
         });
-
-
-        System.out.println();
     }
-
-   /// public static double getSum(Map<String,Double> banks){
-     //   double sum = 0;
-      //  for (Map.Entry<String, Map<String, Double>> stringMapEntry : banks.entrySet()) {
-      //      for (Map.Entry<String, Double> stringDoubleEntry : stringMapEntry.getValue().entrySet()) {
-     //           sum += stringDoubleEntry.getValue();
-     //       }
-     //   }
-    //    return sum;
-    //}
 
     public static double getHighestBalance(Map<String,Double> banks){
         double maxBalance = Double.MIN_VALUE;
